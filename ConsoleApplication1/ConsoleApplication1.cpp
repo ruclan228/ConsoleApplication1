d9a2654305e5bd185fa74f048153a9ce2ace4424
234967 <234967@KB37-309-C06>
2024-09-24 14:11:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main() {
+	bool a, b;
+	std::cout << " A | B | \"И\" | \"ИЛИ\" | \"И-НЕ\" | \"ИЛИ-НЕ\" | \"ИСКЛЮЧАЮЩЕЕ ИЛИ\"" << std::endl;
+
+		a = 0, b = 0;
+		printf(" %d | %d | %d | %d | %d | %d | %d \n", a, b, a and b, a or b, not (a and b), not (a or b), a != b);
+
+		a = 0, b = 1;
+		printf(" %d | %d | %d | %d | %d | %d | %d \n", a, b, a and b, a or b, not (a and b), not (a or b), a != b);
+
+		a = 1, b = 0;
+		printf(" %d | %d | %d | %d | %d | %d | %d \n", a, b, a and b, a or b, not (a and b), not (a or b), a != b);
+
+		a = 1, b = 1;
+		printf(" %d | %d | %d | %d | %d | %d | %d \n", a, b, a and b, a or b, not (a and b), not (a or b), a != b);
+
+
+		return 0;
+}
